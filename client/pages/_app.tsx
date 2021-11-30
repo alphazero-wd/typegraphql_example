cadbae88483571d04bb8447062e1fad263385b9a
Alphazero <alphazero25811@gmail.com>
2021-11-30 10:34:06 +0700

    next frontend

---
+++ client/pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from 'next/app';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache(),
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+
+export default MyApp;
---
+++ client/pages/pages/user/signup.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const signup: NextPage = () => {
+  return <div className="container"></div>;
+};
+
+export default signup;
