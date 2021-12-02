f03d63cfc42107dfd3d1715bb9508bb5f8a43ace
Alphazero <alphazero25811@gmail.com>
2021-12-02 13:43:51 +0700

    modified:   client/pages/index.tsx

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,22 +1,22 @@
-import type { NextPage } from "next";
-import { useRouter } from "next/router";
-import { useMeQuery } from "../generated/graphql";
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useMeQuery } from '../generated/graphql';
 
 const Home: NextPage = () => {
   const router = useRouter();
-  const { data, error } = useMeQuery();
-  console.log(data, error);
-  // useEffect(() => {
-  //   if (!data) {
-  //     router.push("/user/login");
-  //   }
-  // }, [data]);
+  const { data } = useMeQuery();
+  useEffect(() => {
+    if (!data) {
+      router.push('/user/login');
+    }
+  }, [data]);
   return (
     <div>
       <h3 className="text-center mb-3">Hello {data?.me?.fullName}</h3>
       <p className="mb-3">Email: {data?.me?.email}</p>
-      <p className={`text-${data?.me?.isConfirmed ? "success" : "danger"}`}>
-        You have {data?.me?.isConfirmed ? "" : "not"} been confirmed.
+      <p className={`text-${data?.me?.isConfirmed ? 'success' : 'danger'}`}>
+        You have {data?.me?.isConfirmed ? '' : 'not'} been confirmed.
       </p>
     </div>
   );
